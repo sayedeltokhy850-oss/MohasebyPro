b9b338f10e9c3e74c9798115adedb81ce9e37231
sayedeltokhy850-oss <sayedeltokhy850@gmail.com>
2026-08-26 11:25:49 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,72 @@
 
+const CACHE_NAME = "mahaseby-pro-v1";
+
+const FILES = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
+
+self.addEventListener("install", function (event) {
+
+  event.waitUntil(
+
+    caches.open(CACHE_NAME).then(function (cache) {
+
+      return cache.addAll(FILES);
+
+    })
+
+  );
+
+  self.skipWaiting();
+
+});
+
+
+self.addEventListener("activate", function (event) {
+
+  event.waitUntil(
+
+    caches.keys().then(function (keys) {
+
+      return Promise.all(
+
+        keys
+        .filter(function (key) {
+
+          return key !== CACHE_NAME;
+
+        })
+        .map(function (key) {
+
+          return caches.delete(key);
+
+        })
+
+      );
+
+    })
+
+  );
+
+  self.clients.claim();
+
+});
+
+
+self.addEventListener("fetch", function (event) {
+
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+
+    caches.match(event.request).then(function (cached) {
+
+      return cached || fetch(event.request);
+
+    })
+
+  );
+
+});
